5c969da46d5bbd9ef33101b53862dde19903ba2a
johaolopz <johao.lopz.m@gmail.com>
2022-01-25 16:43:29 -0500

    greensock/gsap used

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUM_COUNT = 10
 
@@ -16,11 +17,18 @@ export const CounterEffect = () => {
 
         //Una vez counter sea 10+ hace el efecto y estiliza con css la consola del navegador
       console.log("%cSe llegó al max","color: red; background-color: black;")
+
+      //ANIMATION WITH GSAP SCREENSOCK, X-Y SLIDE/DURATION/ANIMATION TYPES
+      //Estos métodos son promesas, se puede usar .then
+      gsap.to('h2', {y: -10, duration: 0.2, ease: 'ease.out'}).then(()=>{
+          gsap.to('h2', {y: 0, duration: 1, ease: 'bounce.out'})
+        })
     
     }, [counter]);
     
   return <>
-      <h1>CounterEffect: {counter}</h1>
+      <h1>CounterEffect:</h1>
+      <h2>{counter}</h2>
       <button onClick={handleClick}>
           +1
       </button>
